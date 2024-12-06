d2ac4c657191ce402adab260e99dffa545523e65
KunalKapure05 <kapurekunal61@gmail.com>
2024-12-06 20:53:16 +0530

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import app from './app';
+import dotenv from 'dotenv'
+dotenv.config();
+
+
+
+const StartServer =async()=>{
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port no. : ${PORT}`);
+        
+    })
+
+}
+
+StartServer();
+
